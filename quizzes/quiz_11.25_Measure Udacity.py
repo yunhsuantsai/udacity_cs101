f134d584901c5ae2879ed4fdb0370311522c4b0d
travis.tsai <yunhsuan.tsai@gmail.com>
2017-06-18 01:15:08 +0800

    submit quiz

---
+++ quizzes/quiz_11.25_Measure Udacity.py
@@ -0,0 +1,20 @@
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase 
+# letter 'U'.
+
+def measure_udacity(u):
+    result = 0
+    for i in u:
+        if i[0] == 'U':
+            result += 1
+    return result
+
+
+print (measure_udacity(['Dave','Sebastian','Katy']))
+#>>> 0
+
+print (measure_udacity(['Umika','Umberto']))
+#>>> 2
---
+++ quizzes/quiz_11.26_Find Element.py
@@ -0,0 +1,25 @@
+# Define a procedure, find_element,
+# that takes as its inputs a list
+# and a value of any type, and
+# returns the index of the first
+# element in the input list that
+# matches the value.
+
+# If there is no matching element,
+# return -1.
+
+def find_element(a, b):
+    t = 0
+    for i in a:
+        if i == b:
+            return t
+        else:
+        	t += 1
+    return -1
+
+
+print (find_element([1,2,3],3))
+#>>> 2
+
+print (find_element(['alpha','beta'],'gamma'))
+#>>> -1
